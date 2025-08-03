63505dd4d630102a4959b66c224b2d4fa85fed91
sagaradreddy <sagarad671@gmail.com>
2025-08-04 00:12:10 +0530

    `Update port number in app.ts from 3000 to 4000`

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { connectToDatabase } from './config/database';
 import mediaRoutes from './routes/mediaRoutes';
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
@@ -18,10 +18,6 @@ app.use(cors({
 
 // Database connection
 connectToDatabase();
-// Routes
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to the Media Management API');
-});
 app.use('/api/media/', mediaRoutes);
 
 app.listen(PORT, () => {
